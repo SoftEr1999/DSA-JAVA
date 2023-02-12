3f3a8555beb15eab338cb915a193bb879bb9083c
SoftEr1999 <satyamchoudhary737@gmail.com>
2023-02-12 13:07:48 +0530

    Checking

---
+++ DSA/src/dsa/patterns/AllPatterns.java
@@ -0,0 +1,10 @@
+package dsa.patterns;
+
+public class AllPatterns {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ DSA/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Rupa
+ *
+ */
+module DSA {
+}
